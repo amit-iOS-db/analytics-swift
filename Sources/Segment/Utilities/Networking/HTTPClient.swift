eb38162f377e2c51d00c4c6174a3981b09b2a538
Amit Kumar <amit.kumar@dbdigital.in>
2025-02-13 13:48:17 +0530

    refactor: path removed

--- Sources/Segment/Utilities/Networking/HTTPClient.swift
+++ Sources/Segment/Utilities/Networking/HTTPClient.swift
@@ -53,7 +53,7 @@ public class HTTPClient {
     ///   - completion: The closure executed when done. Passes if the task should be retried or not if failed.
     @discardableResult
     func startBatchUpload(writeKey: String, batch: URL, completion: @escaping (_ result: Result<Bool, Error>) -> Void) -> (any DataTask)? {
-        guard let uploadURL = segmentURL(for: apiHost, path: "/b") else {
+        guard let uploadURL = segmentURL(for: apiHost, path: "") else {
             self.analytics?.reportInternalError(HTTPClientErrors.failedToOpenBatch)
             completion(.failure(HTTPClientErrors.failedToOpenBatch))
             return nil
